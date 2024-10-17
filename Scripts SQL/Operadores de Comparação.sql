669885e2a984fa598991ffa3726f5e6c149d4491
sergiohbsantos201 <sergio.borges2002@gmail.com>
2024-10-17 10:34:20 -0300

    changes

---
+++ Scripts SQL/Operadores de Comparação.sql
@@ -0,0 +1,6 @@
+USE EmpresaTI45;
+/*Operadores de Comparação
+>, >=, <, <=, =, <>, */
+SELECT * FROM cadfun WHERE depto = '2';
+SELECT * FROM cadfun WHERE funcao='vendedora';
+SELECT * FROM cadfun WHERE salario <= 7000;
